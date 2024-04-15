0e2c7cbbcab75e1cee9fc58ccc83f4ee2063e4e0
Jasminerm16 <Jasminerm16@gmail.com>
2024-04-15 16:04:45 -0500

    classobjectexamplepart1

---
+++ Lessons/Lesson11.js
@@ -0,0 +1,55 @@
+const myHouse = new object();
+// console.log(myh\House);
+myHouse.foundation = "concrete";
+myHouse.windowsize ="20";
+myHouse.doorcolor = "white";
+myHouse.rooftype = "composite";
+// console.log(myHouse);
+// console.log(myHouse.rooftype);
+
+const house= {
+    foundationstyle: "Dirt",
+    windowInchesize: "30",
+    housecolor: "Tan",
+    rooftiletype: "Shingle"
+};
+//console.log(house);
+house.foundationstyle: "Concrete";
+house["housecolor"] = "white";
+console.log(house);
+// console.log(house["housecoloe"]);
+
+const somethingj ={};
+const str = "somestring";
+const rand = Math.random();
+const myObj = {};
+const rand 
+somethingObj.type ="Lorem ipsum dolor sit amet";
+
+somethinObj["date issued"]- "key has a sapce";
+somethingObj[str]= "key is str variable.";
+somethingObj[rand]= "key is a rand variable";
+somethingObj[myObj]= "key is myObj variable";
+somethingObj[""]= "key is a empty string";
+// console,log(somethingObj);
+//console.log(something)Obj.somestring);
+
+
+const alpha=Math.random();
+const beta = "My beta";
+
+
+const happy = {
+[cat] = "some number",
+[beta]: "Just another string"
+};
+// console.log()
+
+
+
+const Showsomething(obj, objname)==> {
+    const results ="";
+    for(const key in obj){
+        if(object,hasOwn(obj,)){
+                result += `${objName}.${obj[key]}\n`;
+                
